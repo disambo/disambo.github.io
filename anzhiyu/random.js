d967c27c1c0852bec0d6f9fc9da4feed9d779948
Firetheft <97147055+Firetheft@users.noreply.github.com>
2023-10-06 23:13:37 +0800

    1006

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/09/15/SSRT-VS-SSGI/","2023/09/10/how-to-use-Tris-to-Quads-Ex/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/09/15/SSRT-VS-SSGI/","2023/10/01/Rig-anything/","2023/09/10/how-to-use-Tris-to-Quads-Ex/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
